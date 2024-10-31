693a4a04d3353301629db1887a023ebfce7d988b
359jxf <1151976118@qq.com>
2024-10-31 19:56:03 +0800

    update background theme

--- src/components/BackgroundTheme.vue
+++ src/components/BackgroundTheme.vue
@@ -27,14 +27,14 @@
   // 定义主题数组
   const themes: Theme[] = [
     {
+      background: '#231F20',
+      color: '#FFF',
+      primaryColor: '#BB4430'
+    },
+    {
       background: '#1A1A2E',
       color: '#FFFFFF',
       primaryColor: '#0F3460'
-    },
-    {
-      background: '#461220',
-      color: '#FFFFFF',
-      primaryColor: '#E94560'
     },
     {
       background: '#192A51',
@@ -42,19 +42,19 @@
       primaryColor: '#967AA1'
     },
     {
-      background: '#F7B267',
-      color: '#000000',
-      primaryColor: '#F4845F'
+      background: '#513533',
+      color: '#FFFFFF',
+      primaryColor: '#e2b9b3'
     },
     {
-      background: '#F25F5C',
+      background: '#e3e1c8',
       color: '#000000',
-      primaryColor: '#642B36'
+      primaryColor: '#518f8b'
     },
     {
-      background: '#231F20',
-      color: '#FFF',
-      primaryColor: '#BB4430'
+      background: '#ebedec',
+      color: '#000000',
+      primaryColor: '#30A9DE'
     }
   ];
   
